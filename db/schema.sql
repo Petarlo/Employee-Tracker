8ea84d4f693a22b8cc20db5020f6c0c8c20fbbe2
peta swan <petaswan986@gmail.com>
2023-11-11 11:38:07 +1100

    schema & seed files update

--- db/schema.sql
+++ db/schema.sql
@@ -5,20 +5,20 @@ USE employee_db;
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+  emp_name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
+CREATE TABLE emp_role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
-  department_id INT NOT NULL AUTO_INCREMENT
+  department_id INT NOT NULL 
 );
 
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT AUTO_INCREMENT,
+  role_id INT,
   manager_id INT
 );
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const sequelize = require('./config/connection');
+const inquirer = require('inquirer');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,5 +9,5 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 sequelize.sync().then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
 });
